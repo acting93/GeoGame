2d3070f4e248792a9928930c6f8fcaaa0ca83df5
acting93 <53194530+acting93@users.noreply.github.com>
2019-08-11 23:46:46 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$(window).resize(function(){
+ if($(window).width() < 300){
+   $('div').css({width:"100%",height:"130px",margin:"0px"});
+   $('h4').css({fontSize:"10px"});     
+   $('h2').css({fontSize:"12px"});     
+   $('p').css({fontSize:"10px"});
+   $('header').css({height:"100px",borderRadius:"0"})
+   $('li').css({width:"100%",display:"block",marginLeft:"0px",marginBottom:"5px",fontSize:"10px" })     
+   $(copyright).css({marginTop:"5px"});
+   $(logo).css({position:"relative",margin:"0 auto",width:"60px",height:"60px"});
+   $(socials).hide();     
+   $(socials2).show();
+   $(imgfooter).css({marginLeft:"0 auto",width:"40px",height:"40px"});
+   $('footer').css({height:"100px",borderRadius:"0"});
+   $('.container-fluid').css({margin:"0px"});
+ }   
+ else if($(window).width() < 500){
+     $('div').removeAttr('style');
+     $('ul').css({marginLeft:"0px",padding:"0px"})
+     $('li').css({width:"100%",display:"block",marginLeft:"0px",marginBottom:"5px",fontSize:"15px" })
+     $('h4').css({fontSize:"15px"});     
+     $('h4').css({fontSize:"20px"});     
+     $('p').css({fontSize:"15px"});
+     $(logo).css({position:"relative",margin:"0 auto",width:"120px",height:"120px"});
+     $('header').css({display:"block",textAlign:"center",borderRadius:"0"});
+     $('footer').css({height:"180px",borderRadius:"0"});
+     $(imgfooter).css({marginLeft:"0 auto",width:"80px",height:"80px"});
+     $(geogame).hide();
+     $(socials).hide();
+     $(socials2).show();
+     $(copyright).css({marginTop:"5px"});
+     
+ }
+ else{
+     $('div').removeAttr('style');
+     $('section').removeAttr('style');
+     $('ul').removeAttr('style');
+     $('li').removeAttr('style');
+     $('h4').removeAttr('style');  
+     $('p').removeAttr('style');
+     $('header').removeAttr('style');
+     $('footer').removeAttr('style');
+     $(imgfooter).removeAttr('style');
+     $(socials2).removeAttr('style');
+     $(follow).removeAttr('style');
+     $(copyright).removeAttr('style');
+     $(logo).removeAttr('style');
+     $(geogame).show();
+ }    
+});    
